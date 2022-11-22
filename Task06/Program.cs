938e2dc4cec5c619f04d1c37951a0bd7738da72d
SeleznevAS <seleznevanatserg@yahoo.com>
2022-11-22 20:17:46 +0300

    Task06 added

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿//**Задача 6:** Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter your number: ");
+int num = int.Parse(Console.ReadLine ());
+int remainder = num % 2;
+if (remainder == 0)
+{
+	Console.WriteLine("Yes, it`s even number");
+}
+else
+{
+	Console.WriteLine("NO. it`s uneven number");
+}
